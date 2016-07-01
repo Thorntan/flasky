aee3cc4f0c228c1a8bf83176cba9e02763a07996
VIVA <anotherdaylee@126.com>
2016-07-01 11:54:26 +0800

    Update database.py
    create a DataBase class

--- app/database.py
+++ app/database.py
@@ -6,65 +6,67 @@ from MySQLdb.cursors import DictCursor
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-MYSQL_HOST = 'localhost'
-MYSQL_PORT = 3306
-MYSQL_USER = 'root'
-MYSQL_PWD = 'admin'
-MYSQL_DB ='yanlihua'
 
+class DataBase:
 
-def GetConnection():
-    conn = MySQLdb.connect(host=MYSQL_HOST, user=MYSQL_USER, passwd=MYSQL_PWD, db=MYSQL_DB, charset="utf8")
-    return conn
+    host = ''
+    port = ''
+    user = ''
+    passwd = ''
+    db = ''
 
+    def __int__(self, host, port, db, user, passwd):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.passwd = passwd
+        self.db = db
 
-def ExecuteSQL(sql, args = None):
-    ret = 0
-    try:
-        conn = GetConnection()
-        cur = conn.cursor()
+    def __get_connection(self):
+        conn = MySQLdb.connect(host=self.host, port=self.port, user=self.user, passwd=self.pwd, db=self.db,
+                               charset="utf8")
+        return conn
 
-        ret = cur.execute(sql, args)
-        conn.commit()
-        cur.close()
-        conn.close()
-    except MySQLdb.Error, e:
-        print("ExecuteSQL error: %s" % str(e))
-        return False
-    return ret
+    def execute(self, sql, args=None):
+        ret = 0
+        try:
+            conn = self.__get_connection()
+            cur = conn.cursor()
+            ret = cur.execute(sql, args)
+            conn.commit()
+            cur.close()
+            conn.close()
+        except MySQLdb.Error, e:
+            print("ExecuteSQL error: %s" % str(e))
+            return False
+        return ret
 
+    def executemany(self, sql, args=None):
+        ret = 0
+        try:
+            conn = self.__get_connection()
+            cur = conn.cursor()
+            ret = cur.executemany(sql, args)
+            conn.commit()
+            cur.close()
+            cur.close()
+        except MySQLdb.Error, e:
+            print("ExecuteSQLs error: %s" % str(e))
+            return False
+        return ret
 
-def ExecuteSQLs(sql, args = None):
-    ret = 0
-    try:
-        conn = GetConnection()
-        cur = conn.cursor()
-
-        ret = cur.executemany(sql, args)
-        conn.commit()
-        cur.close()
-        cur.close()
-    except MySQLdb.Error, e:
-        print("ExecuteSQLs error: %s" %str(e))
-        return False
-    return ret
-
-
-def QueryBySQL(sql, args = None, size = None):
-    results = []
-    try:
-        conn = GetConnection()
-        cur = conn.cursor(cursorclass = DictCursor)
-
-        cur.execute(sql, args)
-        rs = cur.fetchall()
-        for row in rs :
-            results.append(row)
-    except MySQLdb.Error, e:
-        print("QueryBySQL error: %s" %str(e))
-        return None
-    finally:
-        cur.close()
-
-    return results
-
+    def fetch(sql, args=None, size=None):
+        results = []
+        try:
+            conn = self.__get_connection()
+            cur = conn.cursor(cursorclass=DictCursor)
+            cur.execute(sql, args)
+            rs = cur.fetchall()
+            for row in rs:
+                results.append(row)
+        except MySQLdb.Error, e:
+            print("QueryBySQL error: %s" % str(e))
+            return None
+        finally:
+            cur.close()
+        return results
